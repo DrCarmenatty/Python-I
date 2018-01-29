44f6178dd1f19b630b313ccbf0845dfe6c1115bb
DrCarmenatty <35577020+DrCarmenatty@users.noreply.github.com>
2018-01-29 10:15:53 -0500

    Add files via upload

---
+++ Apprentice_Activities_v2.py
@@ -0,0 +1,21 @@
+def activity01(num1):
+	'''Determine if an input number is Even or Odd'''
+	if num1 % 2 <>0 then:
+		print('This is an even number: %d' %num1)
+    return
+
+def activity02(iv_one, iv_two):
+	'''Return the sum of two input values'''
+	return iv_one + iv_two
+
+def activity03(num_list):
+	'''Given a list of integers, count how many are even'''
+	for i in num_list:
+		if i % 2 ==0 then:
+			c +=1
+	return c
+
+def activity04(input_string):
+	'''Return the input string, backward'''
+	return input_string [::-1]
+	
